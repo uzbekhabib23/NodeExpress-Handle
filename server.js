36d65e53cb5e7fd7104b5bdc90d6cd44f4cd6a77
makhsumbek karamatov <uzbekhabib23@gmail.com>
2019-10-16 13:54:05 -0500

    Got the port connected to the server

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
+// Dependencies
 var express = require("express");
+var exphbs = require("express-handlebars");
 
-// Sets up the Express App
-// =============================================================
+// Create an instance of the express app.
 var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 5000;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Static directory to be served
-app.use(express.static("app/public"));
+// Data
 
 // Routes
-// =============================================================
-require("./app/routes/api-routes.js")(app);
+app.get("/weekday", function(req, res) {
+  res.render("index", lunches[0]);
+});
 
-// Starts the server to begin listening
-// =============================================================
+app.get("/weekend", function(req, res) {
+  res.render("index", lunches[1]);
+});
+
+app.get("/lunches", function(req, res) {
+  res.render("all-lunches", {
+    foods: burgers,
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
