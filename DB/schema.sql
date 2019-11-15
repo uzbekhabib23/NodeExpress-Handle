f5b2df30ca00b2d705685ffd6cf241fe3c98ac74
makhsumbek karamatov <uzbekhabib23@gmail.com>
2019-11-14 20:59:05 -0600

    checking and fixing my code

--- DB/schema.sql
+++ DB/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 
 CREATE TABLE burger 
 (
-    id INT auto_increment not NULL,
+    id INT AUTO_INCRAMENT not NULL,
     burger_name varchar(255) NOT NULL, 
     devoured boolean NOT NULL,
     date TIMESTAMP NOT NULL,
